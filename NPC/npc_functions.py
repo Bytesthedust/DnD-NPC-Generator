211e1cea818aabdd3b05069df599eca91a4c237c
Bytesthedust <47130778+Bytesthedust@users.noreply.github.com>
2024-11-29 21:20:18 -0800

    Update npc_functions.py
    Removing defunct comments

--- NPC/npc_functions.py
+++ NPC/npc_functions.py
@@ -58,53 +58,6 @@ def generatePersonality():
     persona_format = "{}, {}, {}".format(perList[0], perList[1], perList[2])
     return persona_format
 
-
-#NAME GENERATION
-
-#Experimental Name Generator w/ Markov Chains
-
-# def train_markov_chain(names):
-#     transitions = {}
-#     for name in names:
-#         for i in range(len(name)-1):
-#             current_char, next_char = name[i], name[i+1]
-#             transitions.setdefault(current_char, []).append(next_char)
-    
-#     #normalize transition probabilities
-#     for current_char, next_chars in transitions.items():
-#         total_transitions = len(next_chars)
-#         transitions[current_char] = {char: next_chars.count(char)/ total_transitions for char in set(next_chars)}
-
-#     return transitions
-# #Experimental
-# def generateName():
-#     seed(None)
-#     length = 10
-#     firstNameFile = open("python\\NPC\\textFiles\\nameList.txt")
-#     firstNameLines = firstNameFile.readlines()
-#     firstNameModel = train_markov_chain(firstNameLines)
-#     FirstCurrent_char = choice(list(firstNameModel.keys()))
-#     FirstGen = FirstCurrent_char
-
-#     for _ in range(length - 1):
-#         next_char_dict = firstNameModel.get(FirstCurrent_char, {})
-
-#         if not next_char_dict:
-#             break
-#         next_char = choices(list(next_char_dict.keys()), weights=next_char_dict.values())[0]
-#         FirstGen += next_char
-#         FirstCurrent_char = next_char
-#     firstNameFile.close()
-    
-#     surnameFile = open("python\\NPC\\textFiles\\surname.txt")
-#     surnameLines = surnameFile.readlines()
-#     surname = choice(surnameLines).strip()
-#     surnameFile.close()
-
-#     name = "{} {}".format(FirstGen, surname) #formatting the final name
-#     return name
-
-
 def generateName():
     """returns a name as a string"""
     #opens file containing first names
